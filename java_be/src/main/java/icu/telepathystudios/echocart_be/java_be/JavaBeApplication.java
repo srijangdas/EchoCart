942d6bc51db3ac4fc4f828da577211e7ef7a961c
srijangdas <shika781@proton.me>
2026-02-09 16:58:20 +0530

    Added Java Springboot

---
+++ java_be/src/main/java/icu/telepathystudios/echocart_be/java_be/JavaBeApplication.java
@@ -0,0 +1,13 @@
+package icu.telepathystudios.echocart_be.java_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBeApplication.class, args);
+	}
+
+}
---
+++ java_be/src/test/java/icu/telepathystudios/echocart_be/java_be/JavaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.telepathystudios.echocart_be.java_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
