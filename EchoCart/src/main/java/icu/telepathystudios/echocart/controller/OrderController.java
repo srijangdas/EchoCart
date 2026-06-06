3e738f3b057bc580178faa4c7ed58a6450b84467
srijangdas <shika781@proton.me>
2026-06-06 20:02:12 +0530

    Added Documentation

--- EchoCart/src/main/java/icu/telepathystudios/echocart/controller/OrderController.java
+++ EchoCart/src/main/java/icu/telepathystudios/echocart/controller/OrderController.java
@@ -3,12 +3,19 @@ package icu.telepathystudios.echocart.controller;
 import icu.telepathystudios.echocart.dto.order.CreateOrderRequest;
 import icu.telepathystudios.echocart.dto.order.OrderResponse;
 import icu.telepathystudios.echocart.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
+@Tag(name="Orders")
 @RestController
 @RequestMapping("/api/orders")
 @CrossOrigin("*")
@@ -16,6 +23,8 @@ import java.util.UUID;
 public class OrderController {
     private final OrderService orderService;
 
+    @Operation(summary = "Create Order")
+    @Parameter(name="token", in = ParameterIn.HEADER, required = true)
     @PostMapping("/")
     public OrderResponse createOrder(@RequestBody CreateOrderRequest request){
         return orderService.createOrder(request);
--- EchoCart/src/main/java/icu/telepathystudios/echocart/dto/order/CreateOrderRequest.java
+++ EchoCart/src/main/java/icu/telepathystudios/echocart/dto/order/CreateOrderRequest.java
@@ -1,11 +1,33 @@
 package icu.telepathystudios.echocart.dto.order;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import tools.jackson.databind.node.ObjectNode;
 
 @Getter
 @Setter
+@Schema(
+        type = "object",
+        example = """
+        {
+        "orderJson": {
+          "items": [
+            {
+              "id": 1,
+              "name": "Potato",
+              "weight": "2kg",
+              "status": "PENDING"
+            }
+          ],
+          "deliveryAddress": "Salt Lake, Kolkata",
+          "specialInstructions": "Fresh vegetables only",
+          "source": "voice"
+        },
+        "estimatedPrice": 69.99
+        }
+        """
+)
 public class CreateOrderRequest {
     private ObjectNode orderJson;
     private double estimatedPrice;
