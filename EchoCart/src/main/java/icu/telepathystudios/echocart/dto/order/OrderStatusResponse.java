622f81c0070b18331d3958d825f27466b48174ba
srijangdas <shika781@proton.me>
2026-07-09 22:21:20 +0530

    Added Endpoint for Service

---
+++ EchoCart/src/main/java/icu/telepathystudios/echocart/dto/order/OrderStatusResponse.java
@@ -0,0 +1,12 @@
+package icu.telepathystudios.echocart.dto.order;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class OrderStatus {
+    private String status;
+}
